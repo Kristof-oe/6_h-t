e221ef281c471ec332e096d0baeb08c7c129d269
Kristof-ay <107989101+Kristof-oe@users.noreply.github.com>
2023-10-25 14:34:53 +0200

    Add project files.

---
+++ 6-het_th69ls/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _6_het_th69ls
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
